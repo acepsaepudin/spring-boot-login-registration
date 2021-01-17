5fb4c628bd2fcb252c2bc94b340dcc475f003c9d
Acep Saepudin <qwertynesia@gmail.com>
2021-01-18 00:24:29 +0700

    Success hit registration endpoint and bypass spring security

---
+++ src/main/java/id/web/acep/springloginregister/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package id.web.acep.springloginregister.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/id/web/acep/springloginregister/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package id.web.acep.springloginregister.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest requst) {
+        return "works!";
+    }
+}
