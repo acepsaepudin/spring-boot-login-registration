a6d1aa61ca8a2e931cf108ad6ea36e4c55bb47fb
Acep Saepudin <qwertynesia@gmail.com>
2021-01-17 22:59:47 +0700

    import from spring initializer

---
+++ src/main/java/id/web/acep/springloginregister/SpringLoginRegisterApplication.java
@@ -0,0 +1,13 @@
+package id.web.acep.springloginregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/web/acep/springloginregister/SpringLoginRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package id.web.acep.springloginregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
