a8e3b38d38ff404221cd24725225a039b6195d44
Acep Saepudin <qwertynesia@gmail.com>
2021-01-18 00:58:23 +0700

    Success register new user without email verification

--- src/main/java/id/web/acep/springloginregister/registration/RegistrationService.java
+++ src/main/java/id/web/acep/springloginregister/registration/RegistrationService.java
@@ -1,10 +1,34 @@
 package id.web.acep.springloginregister.registration;
 
+import id.web.acep.springloginregister.appuser.AppUser;
+import id.web.acep.springloginregister.appuser.AppUserRole;
+import id.web.acep.springloginregister.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
-    public String register(RegistrationRequest requst) {
-        return "works!";
+
+    private EmailValidatorService emailValidatorService;
+
+    private final AppUserService appUserService;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidatorService.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email is not valid.");
+        }
+
+        return appUserService.signUpUser(
+            new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+            )
+        );
     }
 }
